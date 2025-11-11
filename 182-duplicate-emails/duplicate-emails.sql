888c7eb5353ea221e6d84f046cfcda65715796dc
Suraj R <155443432+suraj-vasishtha@users.noreply.github.com>
2025-11-11 15:51:04 +0530

    Time: 488 ms (26.21%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,5 @@
 /* Write your PL/SQL query statement below */
-select email as Email
-from Person
-group by email
-having count(*)>1;
+select email 
+from Person 
+group by email 
+having count(email)>1;
